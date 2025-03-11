539ffc377bdaebe30983e2641d053cf1f7423b09
Shelton Tolbert <tolbert.shelton@gmail.com>
2025-03-10 20:08:11 -0700

    Udate script to use absolute paths and improve fzf options
    - Changed relative paths to absolute paths using SCRIPTDIR variable.
    - Added FZF_LABEL and MENU_HEADER variables for fzf customization.
    - Included additional fzf options for better user experience.
    - Added handling for optional query argument.

--- dfm.sh
+++ dfm.sh
@@ -1,13 +1,27 @@
 #!/bin/bash
 
+SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 # Check if the ./lib directory exists
-if [ ! -d "./lib" ]; then
+if [ ! -d "$SCRIPTDIR/lib" ]; then
   echo "Directory ./lib does not exist."
   exit 1
 fi
 
+FZF_LABEL=" ShelOS 0.0.1b "
+MENU_HEADER="Tools"
+
+#Set to first arg or ''
+if [ -z "$1" ]; then
+  QUERY=""
+else
+  QUERY="$1"
+fi
+
 # List all .sh files in the ./lib directory, pipe them into fzf for selection
-selected_script=$(find ./lib -type f -name "*.sh" | fzf)
+selected_script=$(find "$SCRIPTDIR/lib" -type f -name "main.sh" | \
+  fzf --query="$QUERY" --select-1 --border="rounded" --border-label="$FZF_LABEL" --border-label-pos="0" --header="$MENU_HEADER" --delimiter="/" --with-nth='{-2}'
+)
 
 # Check if a script was selected
 if [ -z "$selected_script" ]; then
