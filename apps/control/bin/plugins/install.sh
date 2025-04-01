eb46f67ef428cd0ea3af1c47a3cfa4a9f55640c6
Shelton Tolbert <tolbert.shelton@gmail.com>
2025-03-31 20:19:29 -0700

    init plugins structure

---
+++ apps/control/bin/plugins/install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+# @doc
+#
+# # A script used to install plugins
+#
+# Plugins are defined in the .config.yaml file using the following format:
+#
+# # ```yaml
+# - plugins:
+#  - url: <githug_plugin_url>
+#    - name?: <plugin_name>
+#    - opts?: <plugin_options>
+#    - config_file?: <$HOME/.config/<plugin_name>.yaml> 
+#  ```
+
+
